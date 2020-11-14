08e0a7cc0035ca69bebbb470a2e5b161cb8b4573
Purinut Thedwichienchai <purinatth@gmail.com>
2020-11-14 23:27:55 +0700

    fixed merge conflict

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
 import {AuthModule} from './auth/auth.module'
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [AuthModule],
-  controllers: [AppController],
+  controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
 export class AppModule {}
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,9 +1,12 @@
 import {
     Controller,
     Get,
-    Param
+    Param,
+    Post,
+    Body
 } from '@nestjs/common';
 import { UsersService } from  './users.service';
+import { CreateUserDto } from './users.dto';
 // import { ApiTags } from '@nestjs/swagger';
 
 // @ApiTags('Users')
@@ -14,5 +17,25 @@ export class UsersController {
     @Get(':userId')
     async getUserById(@Param('userId') userId: number) {
         return this.userService.getUserById(userId);
+    }
+
+    @Get('id/:userId')
+    async getById(@Param('userId') userId: number) {
+        return this.userService.findById(userId);
+    }
+
+    @Get('name/:username')
+    async getByName(@Param('username') username: string) {
+        return this.userService.findOne(username);
+    }
+
+    @Get('/')
+    async getAll() {
+        return this.userService.getAll();
+    }
+
+    @Post('/')
+    async editById(@Body() userDto: CreateUserDto) {
+        return this.userService.editById(userDto);
     }
 }
