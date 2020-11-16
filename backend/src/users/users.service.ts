9fc846ac53f06c412b472d9a920a6369c20a31a5
Purinut Thedwichienchai <purinatth@gmail.com>
2020-11-16 15:07:38 +0700

    remove mocked up database

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -15,28 +15,7 @@ export class UsersService {
     constructor(
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
-    ) {
-        this.users = [
-            {
-                userId: 1,
-                username: 'john',
-                password: 'changeme',
-                isAdmin: true
-            },
-            {
-                userId: 2,
-                username: 'chris',
-                password: 'secret',
-                isAdmin: false
-            },
-            {
-                userId: 3,
-                username: 'maria',
-                password: 'guess',
-                isAdmin: false
-            },
-        ];
-    }
+    ) {}
 
     async findOne(username: string): Promise<User | undefined> {
         return this.users.find(user => user.username === username);
@@ -46,25 +25,5 @@ export class UsersService {
         const res = await this.userRepository.findOne(userId);
         if (!res) throw new BadRequestException('Invalid User');
         return res;
-    }
-
-    async findById(userId: number): Promise<User> {
-        const res = this.users.find(user => user.userId === userId);
-        if (!res) throw new BadRequestException('Not Found');
-        return res;
-    }
-
-    async editById(userDto: CreateUserDto) {
-        const res = this.users.find(user => user.userId === userDto.userId);
-        this.users.push({
-            userId: userDto.userId,
-            username: userDto.username,
-            password: userDto.password,
-            isAdmin: userDto.isAdmin
-        });
-    }
-
-    async getAll(): Promise<User[]> {
-        return this.users;
     }
 }
