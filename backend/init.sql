cbd0ec3dd0943a1b8d9c2b23c6a4f7344f73b340
Purinut Thedwichienchai <purinatth@gmail.com>
2020-11-18 01:54:26 +0700

    fixed init.sql

--- backend/init.sql
+++ backend/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS User(
 	userId INT,
 	PRIMARY KEY (userId),
 	username VARCHAR(50) NOT NULL UNIQUE,
-	password VARCHAR(50) NOT NULL,
+	password VARCHAR(100) NOT NULL,
 	isAdmin BOOLEAN NOT NULL
 );
 
